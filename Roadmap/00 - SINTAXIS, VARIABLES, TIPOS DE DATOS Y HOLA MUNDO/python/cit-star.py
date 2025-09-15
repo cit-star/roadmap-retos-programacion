d0d99da60d45215e13c73fe384d9e869a3394fc0
Citlally <cit@danil.ai>
2025-09-15 14:01:40 -0600

    Create cit-star.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/cit-star.py
@@ -0,0 +1,21 @@
+# https://python.org
+
+# Comentario en una línea 
+
+""" 
+Esto también 
+es un comentario 
+en varias líneas
+
+"""
+
+
+' ' '  Esto también es un comentario ' ' '
+
+my_variable  = "Mi variable" 
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" #por convención
+
+MY CONSTANT = 
+
